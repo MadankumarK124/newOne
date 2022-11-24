3e9ed0182806e3dbb276ac9f567b7f170d1db977
MADAN KUMAR K <MADAN KUMAR K@LAPTOP-IQ9B34OU>
2022-11-24 17:32:21 +0530

    demo

---
+++ src/test/java/demoTest/newTest.java
@@ -0,0 +1,17 @@
+package demoTest;
+
+import org.testng.annotations.Test;
+
+public class newTest {
+	@Test
+	public void iphone()
+	{
+		System.out.println("iPhone14");
+	}
+	public void iphone1()
+	{
+		System.out.println("iPhone14pro");
+	}
+	
+
+}
